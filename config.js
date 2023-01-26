d4d03a8dc89e325e706bc228dfac04f207d8098d
lnisotmyrealname <123646860+lnisotmyrealname@users.noreply.github.com>
2023-01-26 21:59:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'HELLO ĐẠO!',
+    introDesc: `Hôm nay là ngày đặc biệt của cậu, mình có một món quà nho nhỏ cho cậu nè`,
+    btnIntro: ' click đây nè, hihi',
+    title: 'Trước khi bắt đầu, cậu hãy trả lời một câu hỏi ngắn, just for fun của mình nhé 😙',
+    desc: 'Cậu có yêu tớ không??? ',
+    btnYes: 'Tất nhiên rồi <33',
+    btnNo: 'Xin lỗi cậu, nhưng tớ không yêu cậu đâu :3',
+    question:'Vậy là hôm nay cậu đã chính thức 18 tuổi rồi, đến tầm này rồi mình chỉ biết chúc cậu có sức khỏe tinh thần và thể chất thật tốt để học tốt thi tốt, gặp nhiều thuận lợi và may mắn, sắp tới mà thi xong thì được nhận giấy báo nhập học từ trường đại học cậu mong muốn nha. Hồi tết cậu có chia sẻ với mình cậu có mông lung về bản thân cậu, không biết bây giờ cậu đỡ chưa nhưng tớ chỉ có thể nói với cậu rằng đừng quá lo lắng về nó nhé! Ai trong đời có lúc băn khoăn trước nhiều quyết định và khủng hoảng tìm ra bản thân tuổi 17 này hồi ấy cũng chỉ là một phần của cuộc sống thôi á, mình tin rằng cuộc đời cậu còn rất nhiều điều vui vẻ và thú vị trước mắt nên cậu hãy cứ tự tin, thoải mái và cười nhiều lên nữa nhé. Chúc cậu tuổi 18 tràn đày hạnh phúc và sự tích cực nhaaa <3',
+    btnReply: 'Mình không phải một người ăn nói hoa mỹ cho lắm, có gì sai sot mong cậu bỏ qua nhé. Cậu có điều gì đang trăn trở hay muốn nói với mình không nè?',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Cảm ơn cậu đã chia sẻ và phản hồi nha ',
+    messDesc: 'Một lần nữa chúc cậu có một sinh nhật thật vui vẻ và ấm áp nha',
+    btnAccept: 'Okiiiii nè <3',
+    messLink: 'http://fb.com/profile.phd?id=100089725628060' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
