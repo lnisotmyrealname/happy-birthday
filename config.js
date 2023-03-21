3528d3a20c0a0d9eb6d6515d40e624c2bbe09a0d
lnisotmyrealname <123646860+lnisotmyrealname@users.noreply.github.com>
2023-03-21 23:43:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    introTitle: 'Heluu Đạo! Lại là mình, C đây!',
-    introDesc: 'Hôm nay là sinh nhật cậu nên mình có món quà nho nhỏ này nè. Trước khi bắt đầu cậu trả lời một câu hỏi nho nhỏ của mình nha',
+    introTitle: 'Heluu Đạo! Lại là mình, C đây',
+    introDesc: `Hôm nay là sinh nhật cậu nên mình có món quà nho nhỏ này nè. Trước khi bắt đầu cậu trả lời một câu hỏi nho nhỏ của mình nha`,
     btnIntro: ' oki hihi',
     title: 'Cậu cũng yêu tớ đúng khôngg ? ._.',
     desc: 'Cậu mà thoát web thì mình mặc định câu trả lời là CÓ nhé!!',
@@ -9,8 +9,8 @@ const CONFIG = {
     question:'Chà chà, vậy là hôm nay Đạo đã 18 tuổi rồi nè. Đến tầm này rồi mình chỉ biết chúc cậu có sức khỏe để ôn thi thật tốt, gặp nhiều may mắn đặc biệt là trong phòng thi và sắp tới được nhận giấy báo trúng tuyển của trường cậu mong muốn nhaa. Chúc tuổi 18 của cậu thật hạnh phúc và ý nghĩa giống như tuổi 17 của mình đã rất xinh đẹp và đáng nhớ tới nhường nào khi mình biết tới cậu. Vậy thôi, mình cũng không văn vẻ gì nữa, hôm nay chúc cậu thổi nến vui vẻ bên gia đình và bạn bè nha!',
     btnReply: 'Cậu có điều gì muốn gửi tới mình không <3',
     reply: 'Tớ cũng yêu mến cậu nhiều lắm C à <3<3',
-    mess: 'Ỏ, mình biết mà, cám ơn cậu nhiều lắm nè! Mình yêu cậu! ',
-    messDesc: 'Vậy thui, quà của mình không có gì đặc săc lắm, chỉ có như thế này thui, mong cậu không chê. Again, may your birthday be as blissful and successful as ever. I really dont know when will i lose my feelings for you but I know for sure that my 17-year-old self always be in love with you! ',
+    mess: 'Ỏ, mình biết mà, cám ơn cậu nhiều lắm nè. Mình yêu cậu! ',
+    messDesc: 'Vậy thui, quà của mình không có gì đặc săc lắm, chỉ có như thế này thui, mong cậu không chê. Again, may your birthday be as blissful and successful as ever. I really dont know when will i lose my feelings for you but I know for sure that my 17 year old self always be in love with you! ',
     btnAccept: 'Có một bài hát mình đã nghe ngay sau khi mình fall in love với cậu, mỗi khi nghe bài đó mình lại cảm nhận được cảm xúc của mình ngày hôm đó. Cậu có thể nhấn nghe thử nhé. Mình thích bài này lắm á!',
     messLink: 'https://www.youtube.com/watch?v=iggmiF7DNoM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
