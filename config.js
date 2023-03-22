e7b1880fe229fcbaba5494e32e217c6fafe8f06f
lnisotmyrealname <123646860+lnisotmyrealname@users.noreply.github.com>
2023-03-22 21:37:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     btnYes: 'Có chớ, tớ yêu cậu nè, Yêu C lắm lun <3 ',
     btnNo: 'Không có đâu, tớ không yêu cậu >_<',
     question:'Chà chà, vậy là hôm nay Đạo đã 18 tuổi rồi nè. Đến tầm này rồi mình chỉ biết chúc cậu có sức khỏe để ôn thi thật tốt, gặp nhiều may mắn đặc biệt là trong phòng thi và sắp tới được nhận giấy báo trúng tuyển của trường cậu mong muốn nhaa. Chúc tuổi 18 của cậu thật hạnh phúc và ý nghĩa giống như tuổi 17 của mình đã rất xinh đẹp và đáng nhớ tới nhường nào do một phần khi mình biết tới cậu. Vậy thôi, mình cũng không văn vẻ gì nữa, hôm nay chúc cậu thổi nến vui vẻ bên gia đình và bạn bè nha!',
-    btnReply: 'Cậu có điều gì muốn gửi tới mình không <3',
+    btnReply: 'Cậu nhắn gì đó gửi cho mình đi <3',
     reply: 'Tớ cũng yêu mến cậu nhiều lắm C à <3<3',
     mess: 'Ỏ, mình biết mà, cám ơn cậu nhiều lắm nè. Mình yêu cậu! ',
     messDesc: 'Vậy thui, quà của mình không có gì đặc săc lắm, chỉ có như thế này thui, mong cậu không chê và cám ơn cậu đã lướt đến tận đây . Again, may your birthday be as blissful and successful as ever. I really dont know when will i lose my feelings for you but I know for sure that my 17 year old self always be in love with you! ',
-    btnAccept: 'Có một bài hát mỗi khi nghe mình lại nhớ tới cậu và lần đàu tiên gặp cậu, mình thich bài này lắm á, cậu ắn vào đây nghe thử nhé',
+    btnAccept: 'Có một bài hát mỗi khi nghe mình lại nhớ tới cậu và lần đầu tiên gặp cậu, mình thich bài này lắm á, cậu ắn vào đây nghe thử nhé',
     messLink: 'https://www.youtube.com/watch?v=iggmiF7DNoM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
